e56ce727e6d6c4da32e63d5270d6c7406ea6ac63
Anastasia Vdovichenko <anavdovichenko@gmail.com>
2022-08-18 19:23:38 +0300

    Добавила новую разметку

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -8,3 +8,54 @@ $(document).ready(function() {
 
   });
 });
+
+// Табы в контактах
+
+    $('.j-tabs-link').on('click', function(event) {
+        event.preventDefault();
+
+        $('.j-tabs-link').removeClass('active');
+        $(this).addClass('active');
+
+        let index = $(this).index('.j-tabs-link');
+
+        $('.j-contacts-item').removeClass('active');
+        $('.j-contacts-item').eq(index).addClass('active');
+
+    });
+// фильтр в каталоге
+
+$('.j-catalog-link').on('click', function(event) {
+  event.preventDefault();
+
+  $('.j-catalog-link').removeClass('active');
+  $(this).addClass('active');
+
+  let index = $(this).index('.j-catalog-link');
+
+});
+
+$('.j-catalog-link').on('click', function(event) {
+  event.preventDefault();
+
+  let filterData = $(this).data('filter');
+
+  console.log(filterData);
+
+  if (filterData === 'all') {
+      $('.catalog-item').show();
+      return;
+  }
+
+  $('.catalog-item').each(function() {
+
+      let typeData = $(this).data('type');
+
+      if (filterData === typeData) {
+          $(this).show();
+      } else {
+          $(this).hide();
+      }
+
+  });
+});
